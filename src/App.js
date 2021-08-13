2635a96370639bb20f4bd1cafb6b85e0453da619
DerianM <53027082+DerianMelvin@users.noreply.github.com>
2021-08-14 01:15:35 +0800

    Refactor code to use components

--- src/App.js
+++ src/App.js
@@ -1,54 +1,25 @@
 import "./App.css";
 import { useState } from "react";
-import axios from "axios";
-import { useSelector, useDispatch } from "react-redux";
-import { searchMovies } from "./redux/actions";
+import ModalWindow from "./components/ModalWindow";
+import SearchBar from "./components/SearchBar";
+import MovieList from "./components/MovieList";
 
 function App() {
-  // Redux setup
-  const movies = useSelector((state) => state);
-  const dispatch = useDispatch();
+  // Set modal window display & image
+  const [showModal, setShowModal] = useState(false);
+  const [modalImage, setModalImage] = useState("");
 
-  // Store current input
-  const [query, setQuery] = useState("");
-
-  // Get data from API & store it in redux state
-  const getData = (e) => {
-    e.preventDefault();
-    axios
-      .get(`http://www.omdbapi.com?apikey=faf7e5bb&s=${query}`)
-      .then((res) => dispatch(searchMovies(res.data.Search)));
-  };
-
-  const displayMovieList = () => {
-    if (movies.length === 0) {
-      return <h2>Not seeing anything? Try typing some movie names</h2>;
-    } else {
-      return movies.map((movie) => {
-        return (
-          <div>
-            <img src={movie.Poster} alt={`${movie.Title} Poster`} />
-            <h1>{movie.Title}</h1>
-            <h1>{movie.Year}</h1>
-          </div>
-        );
-      });
-    }
+  const displayModal = (imgSrc) => {
+    setModalImage(imgSrc);
+    setShowModal((prevState) => !prevState);
   };
 
   return (
     <div className="App">
-      <form onSubmit={getData}>
-        <input
-          type="text"
-          placeholder="Search movies..."
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-        />
-        <button>Search</button>
-      </form>
+      {showModal && <ModalWindow imgSrc={modalImage} setShowModal={setShowModal} />}
 
-      {displayMovieList()}
+      <SearchBar />
+      <MovieList displayModal={displayModal} />
     </div>
   );
 }
