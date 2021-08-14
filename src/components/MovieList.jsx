0b99fe5dfb18237b54fac36eb108547ad01389fe
DerianM <53027082+DerianMelvin@users.noreply.github.com>
2021-08-14 11:14:32 +0800

    Add styling for MovieList component - Create MovieList.module.css - Import CSS module to MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,27 +1,35 @@
 import { useSelector } from "react-redux";
+import styles from "../css/MovieList.module.css";
 
 const MovieList = ({ displayModal }) => {
   // Redux setup
   const movies = useSelector((state) => state);
 
-  return movies.length === 0 ? (
-    <h2>Not seeing anything? Try typing some movie names</h2>
-  ) : (
-    movies.map((movie) => {
-      return (
-        <>
-          <div key={movie.imdbID}>
-            <img
-              src={movie.Poster}
-              alt={`${movie.Title} Poster`}
-              onClick={() => displayModal(movie.Poster)}
-            />
-            <h1>{movie.Title}</h1>
-            <h1>{movie.Year}</h1>
-          </div>
-        </>
-      );
-    })
+  return (
+    <div className={styles.container}>
+      {movies.length === 0 ? (
+        <h2 className={styles.empty}>
+          Not seeing anything? Try typing some movie names
+        </h2>
+      ) : (
+        movies.map((movie) => {
+          return (
+            <div className={styles.movie} key={movie.imdbID}>
+              <img
+                className={styles.poster}
+                src={movie.Poster}
+                alt={`${movie.Title} Poster`}
+                onClick={() => displayModal(movie.Poster)}
+              />
+              <div className={styles.info}>
+                <h1>{movie.Title}</h1>
+                <h2 className={styles.year}>{movie.Year}</h2>
+              </div>
+            </div>
+          );
+        })
+      )}
+    </div>
   );
 };
 
