d4ca9882dcf05d069f6d0e8833b9738492d0c55f
DerianM <53027082+DerianMelvin@users.noreply.github.com>
2021-08-14 12:24:06 +0800

    Add actions toggleModal, setModalImage

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -4,3 +4,17 @@ export const searchMovies = (data) => {
     payload: data,
   };
 };
+
+export const toggleModal = (data) => {
+  return {
+    type: "TOGGLE_MODAL",
+    payload: data,
+  };
+};
+
+export const setModalImage = (data) => {
+  return {
+    type: "SET_MODAL_IMAGE",
+    payload: data,
+  };
+};
