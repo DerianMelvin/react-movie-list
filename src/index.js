dda9d7894dc5cb3c25b74001266ff148a1b026aa
DerianM <53027082+DerianMelvin@users.noreply.github.com>
2021-08-14 12:27:43 +0800

    Adjust imported reducer

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import "./index.css";
 import App from "./App";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
-import reducer from "./redux/reducers";
+import allReducers from "./redux/reducers";
 
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
   <React.StrictMode>
