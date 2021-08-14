ed8a58806a06e269fb0c9dd5d0346231bbc1ff17
DerianM <53027082+DerianMelvin@users.noreply.github.com>
2021-08-14 19:58:37 +0800

    Add infinite scrolling functions - Import useEffect from react - Import useSelector from react-redux - Import actions from redux/actions

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,11 +1,17 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
-import { useDispatch } from "react-redux";
-import { searchMovies } from "../redux/actions";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  searchMovies,
+  incrementIndex,
+  appendMovies,
+  resetIndex,
+} from "../redux/actions";
 import styles from "../css/SearchBar.module.css";
 
 const SearchBar = () => {
   // Redux setup
+  const index = useSelector((state) => state.index);
   const dispatch = useDispatch();
 
   // Store current text input
@@ -14,24 +20,88 @@ const SearchBar = () => {
   // Toggle display for empty or error search
   const [error, setError] = useState(false);
 
+  // Toggle to load more data
+  const [loadMore, setLoadMore] = useState(false);
+
+  // Toggle if index has reset
+  const [indexReset, setIndexReset] = useState(false);
+
+  // Listens for scroll behaviour
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  // Checks if loadMore is true
+  useEffect(() => {
+    if (!loadMore) {
+      return;
+    }
+
+    loadData();
+  }, [loadMore]);
+
+  // Checks if indexReset is true
+  useEffect(() => {
+    if (!indexReset) {
+      return;
+    }
+
+    fetchData();
+  }, [indexReset]);
+
+  // Checks if browser window is scrolled to bottom
+  const handleScroll = () => {
+    let difference = document.documentElement.scrollHeight - window.innerHeight;
+    let scrollposition = document.documentElement.scrollTop;
+
+    if (difference - scrollposition <= 1) {
+      dispatch(incrementIndex());
+      setLoadMore(true);
+    }
+
+    return;
+  };
+
+  // Fetch & load the next data
+  const loadData = () => {
+    axios
+      .get(`http://www.omdbapi.com?apikey=faf7e5bb&s=${query}&page=${index}`)
+      .then((res) => dispatch(appendMovies(res.data.Search)))
+      .catch((error) => console.log(error))
+      .then(() => setLoadMore(false));
+  };
+
+  // Update query based on input
+  // Close any error if occured
   const handleInputChange = (e) => {
     setQuery(e.target.value);
     setError(false);
   };
 
-  // Fetch data based on search result & update state
-  const getData = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
+    dispatch(resetIndex());
+    setIndexReset(true);
+  };
+
+  // Fetch data based on search result & update state
+  const fetchData = () => {
     axios
-      .get(`http://www.omdbapi.com?apikey=faf7e5bb&s=${query}`)
+      .get(`http://www.omdbapi.com?apikey=faf7e5bb&s=${query}&page=${index}`)
       .then((res) => dispatch(searchMovies(res.data.Search)))
-      .catch((error) => setError((prevState) => !prevState));
+      .catch((error) => setError((prevState) => !prevState))
+      .then(() => setIndexReset(false));
   };
 
   return (
     <>
-      {error && <div className={styles.error}>There seems to be an error. Try checking your input.</div>}
-      <form className={styles.form} onSubmit={getData}>
+      {error && (
+        <div className={styles.error}>
+          There seems to be an error. Try checking your input.
+        </div>
+      )}
+      <form className={styles.form} onSubmit={handleSubmit}>
         <input
           className={styles.input}
           type="text"
